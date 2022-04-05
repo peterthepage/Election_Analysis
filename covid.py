c39e0e15e37e1af758b1bacc9b0607eda5e1180b
peterthepage <88513858+peterthepage@users.noreply.github.com>
2022-04-05 15:48:27 -0500

    Delete covid.py

--- covid.py
+++
@@ -1,8 +0,0 @@
-from operator import contains
-
-
-covid = int(input('Do you have covid?'))
-if covid contains 'es':
-    print('Time to lick some old folks!')
-else:
-    print('Eh, life is pointless')
